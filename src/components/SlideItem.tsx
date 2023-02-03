0f95aa71fb3453bafb08765d1a9933221ea34913
manjaka <you@example.com>
2023-02-03 15:46:11 +0300

    installs packages and fixes slider card item

--- src/components/SlideItem.tsx
+++ src/components/SlideItem.tsx
@@ -1,33 +1,47 @@
 import {StyleSheet, Text, View, Dimensions} from 'react-native';
 import * as React from 'react';
 const {width, height} = Dimensions.get('screen');
-const SlideItem = ({item}) => {
+
+import {LineChart} from 'react-native-charts-wrapper';
+
+const Chart = ({item}) => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>{item.title}</Text>
-      <Text style={styles.description}>{item.description}</Text>
+    <View style={[styles.container, styles.elevation]}>
+      <LineChart
+        style={styles.chart}
+        data={{
+          dataSets: [{label: 'Spent', values: [{y: 1}, {y: 2}, {y: 1}]}],
+        }}
+      />
     </View>
   );
 };
 
-export default SlideItem;
+export default Chart;
 
 const styles = StyleSheet.create({
   container: {
-    width,
-    height,
-    alignItems: 'center',
+    width: width / 1.2,
+    height: height / 3,
     flex: 1,
-    backgroundColor: 'red',
-    justifyContent: 'center',
+    backgroundColor: 'white',
+    paddingTop: 18,
+    paddingHorizontal: 11,
+    paddingBottom: 21,
+    borderRadius: 13,
+    marginVertical: 20,
+    marginHorizontal: 10,
+  },
+  elevation: {
+    shadowColor: 'black',
+    elevation: 10,
+  },
+  chart: {
+    flex: 1,
   },
   title: {
     fontSize: 24,
     fontWeight: 'bold',
-    color: '#333',
-  },
-  description: {
-    fontSize: 18,
     color: '#333',
   },
 });
